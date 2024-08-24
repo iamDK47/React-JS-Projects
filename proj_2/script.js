e8b26ea2ce075176e3a156f02a66cafcc93a3014
Daniyal <daniyalkamran33@gmail.com>
2024-08-24 21:48:44 +0500

    lol

---
+++ proj_2/script.js
@@ -0,0 +1,17 @@
+const expDate = document.getElementById('expense-date')
+const expCat = document.getElementById('expense-category')
+const expAmount = document.getElementById('expense-amount')
+const expName = document.getElementById('expense-name')
+const button = document.getElementById('#submit')
+const table = document.getElementById('#expense-table')
+
+let T = []
+
+button.addEventListener('click' , calculate)
+
+function calculate(){
+     for (let index = 0; index < T.length; index++) {
+      const element = array[index];
+      
+     }
+}
