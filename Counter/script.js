df654e1e6556e162a732f3939a0a9374e846d6ed
Daniyal <daniyalkamran33@gmail.com>
2024-09-07 17:36:21 +0500

    counter

--- Counter/script.js
+++ Counter/script.js
@@ -5,12 +5,28 @@ const increase = document.getElementById('increase')
 
 let count = 0
 
-increase.addEventListener('click',function(){
+let counter = function() {
    for (let i = 0; i < 5; i++) {}
    count = count + 1;
    number.innerHTML = count
-})
+}
 
-decrease.addEventListener('click',function(){
-   
-})
+let counter1 = function(){
+   if(count > 0){
+      for (let i = 0; i < 5; i++) {}
+      count = count - 1;
+      number.innerHTML = count
+   } 
+   else if(count === 0) {
+      return
+}}
+
+let res = function(){
+      number.innerHTML = '';
+}
+
+increase.addEventListener('click', counter)
+
+decrease.addEventListener('click', counter1)
+
+reset.addEventListener('click', res)
