0d1df68766531c729197d2c75f9196807e7b578c
Daniyal <daniyalkamran33@gmail.com>
2024-09-12 19:44:21 +0500

    m

---
+++ NavResponse/script.js
@@ -0,0 +1,8 @@
+const home = document.getElementById('1')
+const about = document.getElementById('2')
+const projects = document.getElementById('3')
+const contact = document.getElementById('4')
+
+home.addEventListener('click',function(){
+   
+})
