0c9f13e22d0c84cc2c6a3721af3297cb8691f6ba
Daniyal <daniyalkamran33@gmail.com>
2025-01-24 05:22:21 +0500

    test

--- faizi/lol.js
+++ faizi/lol.js
@@ -1,18 +1,47 @@
-const arr = ['a', 'b', 'c', 'd', 'c', 'c', 'b']
+// const array = ['a', 'b', 'c', 'd', 'c', 'c', 'b']
 
-const obj = {}
+// const obj = {}
 
-function lol(){
- for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  
-  obj.element = 
-   
-  } 
-}
-
-// function lol(){
-//  arr.reduce((total, index) => {
-//   return 
-//  },0)
+// function lol() {
+//  for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   obj[element] = obj[element] ? obj[element] + 1 : 1  
+//  }
+//  console.log(obj)
 // }
+// lol()
+
+// const array = ['a', 'b', 'c', 'd', 'c', 'c', 'b']
+
+// const obj = {}
+
+// function lol() {
+//  for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   if(obj[element]){ obj[element] += 1} else { obj[element] = 1}
+//  }
+//  console.log(obj)
+// }
+// lol()
+
+// const array = ['a', 'b', 'c', 'd', 'c', 'c', 'b']
+
+// const obj = {}
+
+// function lol() {
+//  for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   obj[element] = (obj[element] || 1) + 1
+//  }
+//  console.log(obj)
+// }
+// lol()
+
+const array = ['a', 'b', 'c', 'd', 'c', 'c', 'b']
+
+const lol = array.reduce((total,element) => {
+  total[element] = (total[element] || 0) + 1
+  return total
+},{})
+
+console.log(lol)
