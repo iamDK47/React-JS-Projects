6382b77e725743c2646280ee8f5bc36dfe763dad
Daniyal <daniyalkamran33@gmail.com>
2025-01-25 18:06:31 +0500

    todo

--- shop_list/src/App.jsx
+++ shop_list/src/App.jsx
@@ -4,18 +4,48 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [item, setItem] = useState('')
+  const [array, setArray] = useState([])
+  // const [active, setActive] = useState(null)
+
+  const addItem = () => {
+    setArray([...array, item])
+    setItem('')
+    if(item===''){
+      console.log('type something')
+    }
+    }
+
+    const del = (id) => {
+      setArray(array.filter((elem,index) => id !== index))
+    }
 
   return (
     <>
       <div>
         <input
+        value={item}
+        onChange={e => setItem(e.target.value)}
         placeholder='write something'
         type='text'
         ></input>
-        <button>
+        <button
+        onClick={addItem}
+        >
           Add
         </button>
+        <div>
+          {array.map((list,index) => 
+          <div key={index}>
+            {list}
+            <button 
+            onClick={() => del(index) }
+            >
+              X
+            </button>
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
