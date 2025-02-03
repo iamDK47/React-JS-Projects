73337c34337b4200a6ce2ca3f4774e9c3727c92a
Daniyal <daniyalkamran33@gmail.com>
2025-02-03 22:55:22 +0500

    lo

--- videoPlayer/src/App.jsx
+++ videoPlayer/src/App.jsx
@@ -2,13 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeId, setActiveId] = useState(null)
+
+  const change = (id) => {
+    setActiveId(id === activeId ? null : id)
+  }
+
+
+
+  const array = ['cat', 'dog', 'deer', 'snail']
 
   return (
     <>
-     
+    <div>
+     {array.map((name,index) => 
+      <label key={index}>
+        <input
+        type='radio'
+        checked={activeId == index}
+        onChange={() => change(index)}
+        />
+        {name}
+      </label>
+     )}
+    </div>
     </>
   )
 }
 
 export default App
+
