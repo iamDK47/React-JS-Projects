00d2ea1120fab98fbab51c30b8861d811c87fd6f
Daniyal <daniyalkamran33@gmail.com>
2025-02-06 15:50:08 +0500

    mn

--- faizi/lol.js
+++ faizi/lol.js
@@ -53,7 +53,7 @@ const array = ['a', 'b', 'c', 'd', 'c', 'c', 'b']
 function find() {
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
-    obj[element] = obj[element] ? obj[element] + 1 : 1
+    obj[element] = Object.is(obj[element],element) ? + 1 : 1
   }
   console.log(obj)
 }
