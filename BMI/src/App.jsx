d7ec368aa3720445a6d5310ce41de6caa0ccbf46
Daniyal <daniyalkamran33@gmail.com>
2025-02-12 20:35:07 +0500

    a

--- BMI/src/App.jsx
+++ BMI/src/App.jsx
@@ -4,11 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weight, setWeight] = useState(40)
+  const [height, setHeight] = useState(130)
+
+  const BMI = Number(height) + Number(weight)
 
   return (
     <>
-      
+      <div className='main'>
+        <p>
+          BMI CALCULATOR
+        </p>
+        <div>
+          <p> Weight: {weight} kg</p>
+          <input 
+          type = 'range'
+          step={1}
+          min={50}
+          max={300}
+          onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div>
+          <p> Height: {height} cm</p>
+          <input 
+            type = 'range'
+          step={1}
+          min={130}
+          max={300}
+          onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <p>Your BMI is {BMI} </p>
+      </div>
     </>
   )
 }
