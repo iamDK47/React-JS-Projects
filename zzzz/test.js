fc7df4b919c3788a048231dc8a2f3ccdf1432f47
Daniyal <daniyalkamran33@gmail.com>
2025-02-16 21:34:08 +0500

    l

--- zzzz/test.js
+++ zzzz/test.js
@@ -1,14 +1,9 @@
-const myObj = {
- a: 7,
- get b() {
-   return a + 1;
- },
- set c(x) {
-   a = x / 2;
- },
-};
+const a = '4'
+const b = '4'
 
-console.log(myObj.a); 
-console.log(myObj.b); 
-// myObj.c = 50; 
-// console.log(myObj.a); 
+console.log(a == b)
+
+const object1 = { key: "value" };
+const object2 = { key: "value" }
+
+console.log(object1 === object2)
