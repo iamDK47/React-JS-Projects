4615c40a83ff2b91b335127860a1d6046a78dd90
Daniyal <daniyalkamran33@gmail.com>
2025-02-21 21:56:14 +0500

    jk

--- zzzz/test.js
+++ zzzz/test.js
@@ -1,81 +1,28 @@
-// const person = {
-//  firstName: "John",
-//  lastName: "Doe",
-//  id: 5566,
-//  fullName : function() {
-//    return this.firstName + " " + this.lastName;
-//  }
-// };
+// new Promise((resolve) => {
+//   setTimeout( () => resolve('done!') , 1000)
+// }) .then( result => console.log(result))
 
-// // Display data from the object:
-// console.log(person.fullName())
-
-// const person = {
-//  firstName  : "John",
-//  lastName   : "Doe",
-//  id     : 5566,
-//  myFunction : function() {
-//    return this.firstName + '' + this.lastName;
-//  }
-// };
-// const person = {
-//  firstName  : "John",
-//  lastName   : "Doe",
-//  id     : 5566,
-//  myFunction : function() {
-//    return this;
-//  }
-// };
-
-// Display data from the object:
-// console.log(person.myFunction())
-
-// const p = {
-//  firstName : "han",
-//  lastName : "zimmer"
+// function getUser(userId, callback) {
+//  setTimeout(() => {
+//    console.log("User fetched");
+//    callback({ id: userId, name: "Alice" });
+//  }, 1000);
 // }
 
-// console.log(person.myFunction.bind(p))
-// console.log(person.myFunction.call(p))
-
-// const obj = {
-//  someKey: 1, 
-//  outer: function() {
-//     function inner(){
-//       console.log(this);
-//    }     
-//    inner()
-//  },
+// function getOrders(user, callback) {
+//  setTimeout(() => {
+//    console.log("Orders fetched");
+//    callback(["Order 1", "Order 2"]);
+//  }, 1000);
 // }
 
-// obj.outer();     
+// function getOrderDetails(order, callback) {
+//  setTimeout(() => {
+//    console.log("Order details fetched");
+//    callback({ details: "Details of " + order });
+//  }, 1000);
+// }
 
-// setTimeout(() => {
-//  console.log('hello')
-// }, 3000);
-
-function getUser(userId, callback) {
- setTimeout(() => {
-   console.log("User fetched");
-   callback({ id: userId, name: "Alice" });
- }, 1000);
-}
-
-function getOrders(user, callback) {
- setTimeout(() => {
-   console.log("Orders fetched");
-   callback(["Order 1", "Order 2"]);
- }, 1000);
-}
-
-function getOrderDetails(order, callback) {
- setTimeout(() => {
-   console.log("Order details fetched");
-   callback({ details: "Details of " + order });
- }, 1000);
-}
-
-// ❌ Nested callbacks (Callback Hell)
 // getUser(1, (user) => {
 //  getOrders(user, (orders) => {
 //    getOrderDetails(orders[0], (details) => {
@@ -84,4 +31,12 @@ function getOrderDetails(order, callback) {
 //  });
 // });
 
-getUser(1,() => getOrders(1, () => getOrderDetails(1 , () => console.log('lol'))))
+// getUser(1,() => getOrders(1, () => getOrderDetails(1 , () => console.log('lol'))))
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Data received"), 2000);
+  });
+}
+
+fetchData().then(data => console.log(data)).catch(err => console.error(err));
