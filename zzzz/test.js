c204bbad088b58b731ed04f56c5df4bb878d6f99
Daniyal <daniyalkamran33@gmail.com>
2025-02-21 23:47:39 +0500

    a

--- zzzz/test.js
+++ zzzz/test.js
@@ -1,42 +1,6 @@
-// new Promise((resolve) => {
-//   setTimeout( () => resolve('done!') , 1000)
-// }) .then( result => console.log(result))
+const arr = [1,2,3,4,5,6,7]
+const arr1 = [1,2,3,4,5,6]
 
-// function getUser(userId, callback) {
-//  setTimeout(() => {
-//    console.log("User fetched");
-//    callback({ id: userId, name: "Alice" });
-//  }, 1000);
-// }
+const sevenBoom = arr => arr.includes(7) ? 'boom' : 'NO'
 
-// function getOrders(user, callback) {
-//  setTimeout(() => {
-//    console.log("Orders fetched");
-//    callback(["Order 1", "Order 2"]);
-//  }, 1000);
-// }
-
-// function getOrderDetails(order, callback) {
-//  setTimeout(() => {
-//    console.log("Order details fetched");
-//    callback({ details: "Details of " + order });
-//  }, 1000);
-// }
-
-// getUser(1, (user) => {
-//  getOrders(user, (orders) => {
-//    getOrderDetails(orders[0], (details) => {
-//      console.log(details);
-//    });
-//  });
-// });
-
-// getUser(1,() => getOrders(1, () => getOrderDetails(1 , () => console.log('lol'))))
-
-function fetchData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Data received"), 2000);
-  });
-}
-
-fetchData().then(data => console.log(data)).catch(err => console.error(err));
+console.log(sevenBoom(arr))
