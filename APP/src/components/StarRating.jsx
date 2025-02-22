c8bd1cf0d8929324e27e5f32616a7a21d60166e8
Daniyal <daniyalkamran33@gmail.com>
2025-02-22 23:46:04 +0500

    s

--- APP/src/components/StarRating.jsx
+++ APP/src/components/StarRating.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
 import { FaStar } from 'react-icons/fa'
+import './style.css';
 
 function StarRating({star = 5}){
 
  const [rating , setRating] = useState(0)
- const [hover , setHover] = useState(0)
+ const [hover , setHover] = useState(false)
 
  const onMouseEnter = (currentId) => {
   setHover(currentId)
  }
 
+ const onMouseLeave = () => {
+  setHover(null)
+ }
+
  const onRate = (Id) => {
   setRating(setRating)
+  console.log(Id)
  }
 
  const arr = [...Array(star)]
@@ -19,15 +25,20 @@ function StarRating({star = 5}){
  return(
   <> 
   <div>
-   {
-    arr.map((_,index) => 
-    <FaStar 
-    key={index}
-    onMouseEnter={() => onMouseEnter(index)}
-    onClick={() => onRate(index)}
-    />
-    )
-   }
+   {arr.map((_,index) => {
+    index +=  1
+    return(
+     <FaStar 
+     className={ index <= (hover || rating) ? 'active' : 'inActive'}
+     key={index}
+     onMouseEnter={() => onMouseEnter(index )}
+     onMouseLeave={() => onMouseLeave()}
+     onClick={() => onRate(index )}
+     values={rating}
+     />
+     )
+    }
+   )}
   </div>
   </>
  )
