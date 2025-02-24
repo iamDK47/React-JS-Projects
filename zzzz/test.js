65d9c5fd341a0899c7f6f02e23d0e4633a51dd40
Daniyal <daniyalkamran33@gmail.com>
2025-02-24 20:26:21 +0500

    test

--- zzzz/test.js
+++ zzzz/test.js
@@ -1,6 +1,20 @@
-const arr = [1,2,3,4,5,6,7]
-const arr1 = [1,2,3,4,5,6]
+// const arr = [1,2,3,4,5,6,7]
+// const arr1 = [1,2,3,4,5,6]
 
-const sevenBoom = arr => arr.includes(7) ? 'boom' : 'NO'
+// const sevenBoom = arr => arr.includes(7) ? 'boom' : 'NO'
 
-console.log(sevenBoom(arr))
+// console.log(sevenBoom(arr))
+
+const obj1 = { a: "hello" , b : 'hi' , c : {d : 'hey'}}
+
+const obj2 = {... obj1}
+
+// const {a} = obj1
+// console.log(a)
+
+obj2.c.d = "NOOO"
+
+console.log(obj1)
+console.log(obj2)
+
+
