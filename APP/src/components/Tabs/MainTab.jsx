b381ae68ea7ece10e4674abe09f70c44b32a4c57
Daniyal <daniyalkamran33@gmail.com>
2025-03-22 15:26:05 +0500

    editting

--- APP/src/components/Tabs/MainTab.jsx
+++ APP/src/components/Tabs/MainTab.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { act, useState } from 'react'
 import Comp1 from './Comp1'
 import Comp2 from './Comp2'
+import './style.css'
 
 // function MainTab() {
 
@@ -49,21 +50,37 @@ const tabs = [
 
 function MainTab() {
 
-  const [activeTab, setActiveTab] = useState(0)
+  const [activeTab, setActiveTab] = useState(new Array(tabs.length).fill(false))
+  const [content, setContent] = useState(0)
+
+  // [0,0,0]
+
+  console.log(activeTab)
+  const change = (index) => {
+    setActiveTab((prev) => { 
+     let newArr = [...prev]
+      newArr[index] = activeTab[index] === index ? false : index 
+      return newArr
+    })
+    setContent(index)
+  }
+  console.log(activeTab)
 
   return (
     <div>
-      {
-        tabs.map((tab, index) =>
-          <button className='Btn' key={index} onClick={() => setActiveTab(index)}>
+      {tabs.map((tab, index) => {
+        return   <button
+        className={activeTab[index] === index ? 'Active' : 'notActive'}
+        key={index}
+        onClick={() => change(index)}>
             {tab.name}
           </button>
-
+      }
         )}
         <div>
-          {tabs[activeTab].content}
-        </div>
-    
+        {tabs[content].content}
+      </div>
+
     </div>
   )
 }
