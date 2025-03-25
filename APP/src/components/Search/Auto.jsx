6e8f1279103cef5b600500544e22061ab0210e8f
Daniyal <daniyalkamran33@gmail.com>
2025-03-25 13:48:41 +0500

    algo

--- APP/src/components/Search/Auto.jsx
+++ APP/src/components/Search/Auto.jsx
@@ -2,23 +2,58 @@ import React, { use, useEffect, useState } from 'react'
 
 function Auto() {
 
- const [user, setUser] = useState([])
+  const [user, setUser] = useState({})
+  const [names, setNames] = useState([])
+  const [word, setWord] = useState('')
+
+  useEffect(() => {
+    const func = async () => {
+      try {
+        const call = await fetch('https://dummyjson.com/users');
+        if (!call.ok) throw new ("ERROR", call.status);
+        const res = await call.json();
+        setUser(res);
+      } catch (err) {
+        console.log('error', err);
+      }
+    }
+    func()
+    console.log('API CALLED')
+  }, [])
+
+  // console.log(user)
+
+  const algo = (e) => {
+    const newWord = e.target.value
+
+    if (newWord === '') {
+      setNames([])
+      return
+    }
+
+    setNames(() =>
+      user.users?.filter((obj) => obj.firstName.toLowerCase().startsWith(newWord.toLowerCase()))
+      //  user.users?.filter((obj) => obj.firstName.toLowerCase().charAt(0) === newWord)
+    )
+  }
 
 
- useEffect(() => {
-  fetch("https://dummyjson.com/users")
-  .then(response =>  response.json())
-  .then(res => setUser(res))
- }, [])
 
- console.log(user)
+  console.log('comp rendred')
 
   return (
     <div>
- 
-     {user.users.map((user,index) =>
-     <p key={index}>{user.firstName}</p>
-     )}
+      <input
+        onChange={algo}
+        placeholder='Type Name'
+      />
+      <ul>
+        {names.map((obj, index) =>
+          <li key={index}>
+            {obj.firstName}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
