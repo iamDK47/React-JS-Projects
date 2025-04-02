df3cfe0dd8d215082a713b4a352ed5a63b50ac9c
Daniyal <daniyalkamran33@gmail.com>
2025-04-02 19:49:37 +0500

    test

--- APP/src/components/a/Test.jsx
+++ APP/src/components/a/Test.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
 function Test() {
-  const arr = [3,4,5,[5,4],4,[4,[4,[4]]]]
-  // const arrr = arr.flat().length
-  // console.log(arrr)
+const str1 = '100111'
+const str2 = '011110'
 
-  let sum = 0
-  function summ(arr){
-   arr.forEach(item => {
-      if(Array.isArray(item)){
-        summ(item)
-      }else{
-        sum += 1
-      }
-    });
-  }
-  summ(arr)
-  console.log(sum)
+  // for (let i = 0; i < str1.length; i++) {
+  //   const element = array[i];
+  //   if(element != str2[i]){
+  //     str1 = element === '1' ? 
+  //   }
+  // }
+
+  let count = 0
+
+  // function rep() {
+  //   for (let i = 0; i < str1.length; i++) {
+  //     if(str1[i] != str2[i]){
+  //       count = count + 1
+  //     }
+  //   }
+  // }
+  // rep()
+  // console.log(count/2)
+
+  // const num = str1.split('').filter((item,i) => item != str2[i]).length/2
+  // console.log(num)
 
   return (
     <div>
